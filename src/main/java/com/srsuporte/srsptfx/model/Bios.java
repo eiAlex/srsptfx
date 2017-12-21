a4f35a70993353a5f1ee2cd97a1b79c5199320e3
JoaoNormando <joaonormando50@gmail.com>
2017-12-21 16:40:09 -0200

    Classes de negocio (Bios, Placa mae)

--- src/main/java/com/srsuporte/srsptfx/model/Bios.java
+++ src/main/java/com/srsuporte/srsptfx/model/Bios.java
@@ -5,10 +5,40 @@
  */
 package com.srsuporte.srsptfx.model;
 
+import java.util.Calendar;
+
 /**
  *
  * @author joao
  */
 public class Bios {
+
+    private Calendar dataBios;
+    private String versao;
+    private Fabricante fabricante;
+
+    public Calendar getDataBios() {
+        return dataBios;
+    }
+
+    public void setDataBios(Calendar dataBios) {
+        this.dataBios = dataBios;
+    }
+
+    public String getVersao() {
+        return versao;
+    }
+
+    public void setVersao(String versao) {
+        this.versao = versao;
+    }
+
+    public Fabricante getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(Fabricante fabricante) {
+        this.fabricante = fabricante;
+    }
     
 }
---
+++ src/main/java/com/srsuporte/srsptfx/model/PlacaMae.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.srsuporte.srsptfx.model;
+
+/**
+ *
+ * @author joao
+ */
+public class PlacaMae {
+
+    private String descricao;
+    private String modelo;
+    private String nome;
+    private String serial;
+    private String versao;
+
+    private Fabricante fabricante;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSerial() {
+        return serial;
+    }
+
+    public void setSerial(String serial) {
+        this.serial = serial;
+    }
+
+    public String getVersao() {
+        return versao;
+    }
+
+    public void setVersao(String versao) {
+        this.versao = versao;
+    }
+
+    public Fabricante getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(Fabricante fabricante) {
+        this.fabricante = fabricante;
+    }
+
+}
