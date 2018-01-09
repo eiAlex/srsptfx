5a88055ae9231b052bfc57f9ff37e5c0ef4272bb
Alex Junio <alexshox@gmail.com>
2018-01-09 14:38:12 -0200

    where the magic begins! removido redundancia de classes, melhorias no pon e na compactação

--- src/main/java/com/srsuporte/srsptfx/controller/FXMLVBoxMainController.java
+++ src/main/java/com/srsuporte/srsptfx/controller/FXMLVBoxMainController.java
@@ -1,25 +1,11 @@
 package com.srsuporte.srsptfx.controller;
 
 import com.srsuporte.srsptfx.servico.ConstrutorDeTelas;
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.event.EventType;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.effect.DropShadow;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
 /**
@@ -37,12 +23,21 @@ public class FXMLVBoxMainController implements Initializable {
     ConstrutorDeTelas construtorDeTelas = new ConstrutorDeTelas();
     
 
+     @FXML
+    public void ClickButtonConexaoRemota() {
+
+        System.out.println("Click em Configurações!");
+        //buttonAbrirChamado.setDisable(true);
+         construtorDeTelas.contruirPane("/view/FXMLPaneAcessoRemoto.fxml", paneMidEsquerdo);
+
+    } 
+     
     @FXML
     public void ClickButtonConfiguracoes() {
 
         System.out.println("Click em Configurações!");
         //buttonAbrirChamado.setDisable(true);
-        // construtorDeTelas.contruirPane("/view/FXMLPaneNovoChamado.fxml", paneMidEsquerdo);
+         construtorDeTelas.contruirPane("/view/FXMLPaneConfiguracoes.fxml", paneMidEsquerdo);
 
     }
 
@@ -51,7 +46,7 @@ public class FXMLVBoxMainController implements Initializable {
 
         System.out.println("Click em Info Sistema!");
         //buttonAbrirChamado.setDisable(true);
-        // construtorDeTelas.contruirPane("/view/FXMLPaneNovoChamado.fxml", paneMidEsquerdo);
+        construtorDeTelas.contruirPane("/view/FXMLPaneInfoSistema.fxml", paneMidEsquerdo);
 
     }
 
@@ -60,7 +55,7 @@ public class FXMLVBoxMainController implements Initializable {
 
         System.out.println("Click em Sobre!");
         //buttonAbrirChamado.setDisable(true);
-        // construtorDeTelas.contruirPane("/view/FXMLPaneNovoChamado.fxml", paneMidEsquerdo);
+         construtorDeTelas.contruirPane("/view/FXMLPaneSobre.fxml", paneMidEsquerdo);
 
     }
 
@@ -69,7 +64,7 @@ public class FXMLVBoxMainController implements Initializable {
 
         System.out.println("Click em Abrir Chamado!");
         //buttonAbrirChamado.setDisable(true);
-      //  construtorDeTelas.contruirPane("/view/FXMLPaneNovoChamado.fxml", paneMidEsquerdo);
+        construtorDeTelas.contruirPane("/view/FXMLPaneNovoChamado.fxml", paneMidEsquerdo);
 
     }
 
